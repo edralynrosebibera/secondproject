0711d943785da1e8545da962c2f1b99a2aab5af5
Edralyn Rose Bibera <edralynrosebibera@gmail.com>
2025-09-21 23:11:45 +0800

    feat(booksread2): add template (books.html), styles.css, and update settings, urls, and views

--- booksread/booksread2/views.py
+++ booksread/booksread2/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello, this is my app!")
+books = []
+
+def book_list(request):
+    if request.method == "POST":
+        title = request.POST.get("book_title")
+        if title:  
+            books.append(title)
+    return render(request, "books.html", {"books": books})
